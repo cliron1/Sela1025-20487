88d36f6020fbf6f761c640abd7ca7cdb3d356966
liron <liron@flame-ware.com>
2022-01-05 15:37:53 +0200

    First Commit

---
+++ SimpleSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SimpleSite.Models {
+	public class ErrorViewModel {
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Site/Program.cs
@@ -0,0 +1,27 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Site {
+	public class Program {
+		public static void Main(string[] args) {
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((context, config) => {
+					if (context.HostingEnvironment.IsProduction()) {
+						config.AddAzureKeyVault(
+							new Uri(Environment.GetEnvironmentVariable("VaultUri")),
+							new DefaultAzureCredential()
+						);
+					}
+				})
+				.ConfigureWebHostDefaults(webBuilder => {
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
